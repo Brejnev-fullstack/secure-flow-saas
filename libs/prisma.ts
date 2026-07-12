919556beb5c948642ea09a813e35f665f3cddda8
Brejnev Ngondi <brejnevngondi@gmail.com>
2026-07-12 18:31:49 +0200

    feat: setup prisma client

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -7,6 +7,7 @@ const globalForPrisma = global as unknown as {
 };
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
+
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
